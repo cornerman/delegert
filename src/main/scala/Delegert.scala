188840ddeb86be3e4e9c27b0be2048099bf3484b
johannes karoff <johannes@karoff.net>
2016-10-03 17:33:41 +0200

    reflect and generate

--- src/main/scala/Delegert.scala
+++ src/main/scala/Delegert.scala
@@ -3,14 +3,56 @@ package delegert
 import scala.reflect.macros.whitebox.Context
 import scala.language.experimental.macros
 import scala.annotation.{StaticAnnotation, compileTimeOnly}
+import scala.util.{Try => uTry, Success, Failure}
 
-case class ValueAccessor(name: String, typeName: String)
+class DelegertTranslator[C <: Context](val c: C) {
+  import c.universe._
 
-class DelegertTranslator[C <: Context](val context: C) {
-  import context.universe._
+  case class ValueAccessor(name: TermName, typeTree: Tree, initTree: Tree)
 
-  def translate(value: ValueAccessor): Tree = {
-    q"val ${TermName(value.name)}: ${TypeName(value.typeName)} = ${EmptyTree}"
+  def unmodValDefs(tree: Tree): Tree = tree match {
+    case ValDef(_, name, tpe, init) => ValDef(Modifiers(), name, tpe, init)
+    case t => t
+  }
+
+  def translate(value: ValueAccessor, embeddingClass: ClassDef): Tree = {
+    Option(value.typeTree.tpe) match {
+      case Some(tpe) =>
+        val methods = tpe.decls.filter(_.isMethod).map(_.asMethod)
+
+        val ClassDef(mods, name, tparams, Template(parents, self, body)) = embeddingClass
+        val existingMethods = body.flatMap(tree => tree match {
+          case DefDef(_, n, _, _, _, _) => Some(n)
+          case _ => None
+        }).toSet
+
+        val missingMethods = methods.filterNot(m => existingMethods.contains(m.name))
+
+        val methodsImpls = missingMethods.map { method =>
+          val (params, paramNames) = method.paramLists.headOption.map(_.map { param => //TODO multi paramlists
+            val tpe = param.typeSignature
+            val name = TermName(param.name.toString)
+            (q"$name: $tpe", q"$name")
+          }.unzip).getOrElse((List.empty, List.empty))
+
+          q"def ${method.name}(..$params) = ${value.name}.${method.name}(..$paramNames)"
+        }
+
+        ClassDef(mods, name, tparams, Template(parents, self, body ++ methodsImpls))
+      case None => c.abort(c.enclosingPosition, "type tree does not have type")
+    }
+  }
+
+  def translate(utree: Tree, embeddingClass: ClassDef): Tree = {
+    val tree = unmodValDefs(utree)
+    val typedTree = uTry(c.typecheck(tree.duplicate, silent = false, withMacrosDisabled = true))
+    val value = typedTree match {
+      case Success(q"..$mods val $name: $typeTree = $initTree") => ValueAccessor(name, typeTree, initTree)
+      case Success(_) => c.abort(c.enclosingPosition, "delegert only delegates value accessors")
+      case Failure(err) => c.abort(c.enclosingPosition, s"cannot typecheck given expression: $err")
+    }
+
+    translate(value, embeddingClass)
   }
 }
 
@@ -23,17 +65,14 @@ object DelegertMacro {
     import c.universe._
 
     val translator = DelegertTranslator(c)
-    import translator._
-
     val trees = annottees.map(_.tree)
-    val value = trees.headOption map {
-      case q"..$mods val $name: $typeName = ${EmptyTree}" => ValueAccessor(name.toString, typeName.toString)
-      case _ => c.abort(c.enclosingPosition, "delegert only delegates value accessors")
-    } getOrElse {
-      c.abort(c.enclosingPosition, "delegert does not annotate anything")
+    val annottee = trees.head
+    val nextAnnottees = trees.tail.collect {
+      case c: ClassDef => translator.translate(annottee, c)
+      case t => t
     }
 
-    val outputs = translate(value) +: trees.tail
+    val outputs = nextAnnottees
     c.Expr[Any](q"..$outputs")
   }
 }
--- src/test/scala/DelegertSpec.scala
+++ src/test/scala/DelegertSpec.scala
@@ -1,18 +1,69 @@
 import macroni.CompileSpec
 import scala.reflect.runtime.universe._
 
+trait Tret {
+  def a(): String
+  def b(): String
+}
+
+trait Tret2 {
+  def b(): String
+  def c(): String
+}
+
+class TretImpl extends Tret {
+  def a() = "a"
+  def b() = "b"
+}
+
+class Tret2Impl extends Tret2 {
+  def b() = "b2"
+  def c() = "c2"
+}
+
+object Wraps {
+  class TretWrap(@delegert.delegert val inner: Tret) extends Tret {
+    def a() = "myA"
+  }
+
+  class TretWraps(@delegert.delegert val inner: Tret, @delegert.delegert val inner2: Tret2) extends Tret with Tret2 {
+    def a() = "myA"
+  }
+}
+
 class DelegertSpec extends CompileSpec {
+  import Wraps._
+
+  "one delegate" >> {
+    val wrap = new TretWrap(new TretImpl)
+    wrap.a must beEqualTo("myA")
+    wrap.b must beEqualTo("b")
+  }
+
+  "two delegates" >> {
+    val wrap = new TretWraps(new TretImpl, new Tret2Impl)
+    wrap.a must beEqualTo("myA")
+    wrap.b must beEqualTo("b")
+    wrap.c must beEqualTo("c2")
+  }
+
+  // needs own block around macro usage, as current scope is not accessible in macro expansion: not found type Tret
+  // related: https://github.com/aztek/scala-workflow/issues/2
   "works on constructor value" >> {
-    q"""trait Tret; class A(@delegert.delegert val a: Tret)""" must compile
+    q"""
+      trait Tret { def a() = 1 }
+      {
+        class A(@delegert.delegert val inner: Tret) extends Tret
+      }""" must compile.to(containTree(
+        q"""
+          class A(val inner: Tret) extends Tret {
+            def a() = A.this.inner.a()
+          }"""
+      ))
   }
 
-  "works on member value" >> {
-    q"""trait A { @delegert.delegert val a: A }""" must compile
-  }
-
-  "fails on var" >> {
-    q"""trait Tret; class A(@delegert.delegert var a: Tret)""" must abort(
-      "reflective typecheck has failed: delegert only delegates value accessors"
-    )
-  }
+  // TODO: embedding class?
+  // "works on member value" >> {
+  //   q"""{trait Tret { def a() = 1 }; trait A { @delegert.delegert val inner: Tret }}""" must compile
+  // }
 }
